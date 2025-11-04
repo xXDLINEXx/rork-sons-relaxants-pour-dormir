38a39763fc00d92e3da703fb253a7a2abc0103e1
xXDLINEXx <solution1nfopro@gmail.com>
2025-11-03 23:39:10 -0500

    yyy

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ,
-    plugins: ['expo-router/babel'],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      // laisse ceci si tu utilises les chemins relatifs type "@/components"
+      ['module-resolver', {
+        alias: {
+          '@': './',
+        },
+      }],
+    ],
   };
 };
